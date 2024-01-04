36d767e280d646370cd3ac38f5ad8b7c20131bf1
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2024-01-05 00:30:09 +0300

    Implement admin layout for dashboard

--- Feeds/Areas/Administration/Controllers/DashboardController.cs
+++ Feeds/Areas/Administration/Controllers/DashboardController.cs
@@ -1,6 +1,9 @@
+using Feeds.Models;
 using Feeds.Utilities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Feeds.Areas.Administration.Controllers;
 
@@ -8,8 +11,39 @@ namespace Feeds.Areas.Administration.Controllers;
 [Authorize(Roles = nameof(ApplicationUserRoles.Admin))]
 public class DashboardController : Controller
 {
-    public IActionResult Index()
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public DashboardController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
     {
-        return View();
+        // Inject role manager and user manager 
+        _roleManager = roleManager;
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        // Get all users 
+        var users = await _userManager.Users.ToListAsync();
+        var userRolesViewModel = new List<UserRolesViewModel>();
+        foreach (ApplicationUser user in users)
+        {
+            // Get each user details and assigned roles
+            var thisViewModel = new UserRolesViewModel();
+            thisViewModel.UserId = user.Id;
+            thisViewModel.Email = user.Email;
+            thisViewModel.FirstName = user.FirstName;
+            thisViewModel.LastName = user.LastName;
+            // Invoke GetUserRoles method to get user roles
+            thisViewModel.Roles = await GetUserRoles(user);
+            userRolesViewModel.Add(thisViewModel);
+        }
+
+        return View(userRolesViewModel);
+    }
+    
+    private async Task<List<string>> GetUserRoles(ApplicationUser user)
+    {
+        return new List<string>(await _userManager.GetRolesAsync(user));
     }
 }
